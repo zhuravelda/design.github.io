ae6106dee39936ef92631a17aa0bc04b7b6efd59
zhuravelda <zhuravelda@gmail.com>
2019-04-25 08:22:31 +0300

    сверстал слайдер шапки

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,7 @@
 // client-slider
 
 $('.reviews-client').slick({
-  // autoplay: true,
+  autoplay: true,
   infinite: true,
   slidesToShow: 1,
   slidesToScroll: 1,
@@ -20,4 +20,47 @@ $(this).toggleClass("is-active");
 $(".dropdown").click(function() {
   $(".sub-nav").slideToggle();
   $(this).toggleClass("active");
+});
+
+// header-slider
+
+$('.header-slider').slick({
+  // autoplay: true,
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  centerPadding: 0,
+  centerMode: true,
+  dots: true,
+  arrows: false
+});
+
+
+// projects slider
+
+$('.slider').slick({
+  centerMode: true,
+  focusOnSelect: true,
+  centerPadding: '0',
+  slidesToShow: 4,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '0',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '0',
+        slidesToShow: 1
+      }
+    }
+  ]
 });
